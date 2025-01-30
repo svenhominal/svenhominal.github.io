b963e926bac2e0679ad109e9a0f1b6af48cf140b
svenhominal <sven.hominal@epfl.ch>
2025-01-30 11:30:48 +0100

    modif

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return 'Server is running'
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -21,6 +21,10 @@ def upload_file():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
         return 'File uploaded successfully'
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
