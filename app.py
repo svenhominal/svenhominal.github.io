38778d25fcd3129adcc6bc790af4c383b1380726
svenhominal <sven.hominal@epfl.ch>
2025-01-30 11:13:56 +0100

    modif Spark

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return 'Server is running'
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return 'File uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
